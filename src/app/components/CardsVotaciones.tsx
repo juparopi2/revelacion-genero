d1bad6c69a622c7e4f33fb30ee3ffd22cdc5cb4e
Juan Romero <juanpa10210@outlook.com>
2025-01-20 19:59:33 -0500

    feat: add results display for voting and calculate averages by gender

--- src/app/components/CardsVotaciones.tsx
+++ src/app/components/CardsVotaciones.tsx
@@ -6,8 +6,17 @@ import { IconGenderFemale } from "../icons/IconGenderFemale";
 import { cn } from "../lib/utils";
 import { User } from "../context/UserContext";
 
+interface UserResults {
+  id: string;
+  celular: string;
+  puntaje_formulario: number;
+}
+
 export default function CardsVotaciones() {
   const [votaciones, setVotaciones] = useState<User[]>([]);
+  const [resultadosFormulario, setResultadosFormulario] = useState<
+    UserResults[]
+  >([]);
   const countM = votaciones.filter(
     (votacion) => votacion.genero === "M"
   ).length;
@@ -15,24 +24,70 @@ export default function CardsVotaciones() {
     (votacion) => votacion.genero === "F"
   ).length;
 
+  const resultadosPorGenero = (genero: string) => {
+    const resultadosM = resultadosFormulario.filter((resultado) => {
+      const votante = votaciones.find((v) => v.celular === resultado.celular);
+      return votante?.genero === genero;
+    });
+
+
+    const totalPuntaje = resultadosM.reduce(
+      (sum, resultado) => sum + resultado.puntaje_formulario,
+      0
+    );
+
+    const votantesPorGenero = resultadosFormulario.filter((resultado) => {
+      const votante = votaciones.find((v) => v.celular === resultado.celular);
+      return votante?.genero === genero;
+    }).length;
+
+    return countM > 0 ? (totalPuntaje / votantesPorGenero).toFixed(1) : "0";
+  };
+
   useEffect(() => {
     async function fetchVotaciones() {
       const { data, error } = await supabase.from("profiles").select("*");
       if (error) console.log("Error al traer las votaciones", error);
       if (data) {
         setVotaciones(data.filter((votacion) => votacion.genero));
-        //setVotaciones(data);
       }
     }
+    async function fetchResultadosFormulario() {
+      const { data, error } = await supabase.from("resultados").select("*");
+      if (error)
+        console.log("Error al traer los resultados del formulario", error);
+      if (data) {
+        setResultadosFormulario(data);
+      }
+    }
+    fetchResultadosFormulario();
     fetchVotaciones();
   }, []);
   return (
     <div className="flex flex-col gap-4 w-full items-center justify-center z-40 px-4 pb-4">
       <h3 className="text-5xl font-bold w-full text-center">
         {" "}
+        Resultados del formulario:
+      </h3>
+      <p className="text-center italic">
+        {" "}
+        ¡Calculamos el promedio de las respuestas que nos dieron los integrantes
+        de cada equipo!{" "}
+      </p>
+      <p className="text-center italic">
+        {" "}
+        Recuerda que el puntaje máximo era 14 puntos{" "}
+      </p>
+      <ContadorResultados
+        niños={resultadosPorGenero("M")}
+        niñas={resultadosPorGenero("F")}
+      />
+      <h3 className="text-5xl font-bold w-full text-center">
+        {" "}
         Así van las votaciones:
       </h3>
       <ContadorVotaciones niños={countM} niñas={countF} />
+
       <div className="w-full grid grid-flow-dense grid-cols-12 gap-4 h-fit z-40">
         {votaciones.map((votacion) => (
           <Card
@@ -91,6 +146,36 @@ interface ContadorVotacionesProps {
   niñas: number;
 }
 function ContadorVotaciones(props: ContadorVotacionesProps) {
+  const { niños, niñas } = props;
+  return (
+    <div className="z-40 flex flex-row w-full gap-4 items-center justify-center">
+      <div className="w-32 bg-custom-boy rounded-xl p-2 flex flex-col items-center justify-center text-white">
+        <div className="w-full h-full p-2 border-2 border-dashed border-white rounded-lg">
+          <div className="flex flex-row items-center justify-center ">
+            <IconGenderMale width={"4rem"} height={"4rem"} />
+            <p className="text-4xl">{niños}</p>
+          </div>
+          <p className="text-center w-full font-semibold">Niños</p>
+        </div>
+      </div>
+      <div className="w-32 bg-custom-girl rounded-xl p-2 flex flex-col items-center justify-center text-white">
+        <div className="w-full h-full p-2 border-2 border-dashed border-white rounded-lg">
+          <div className="flex flex-row items-center justify-center ">
+            <IconGenderFemale width={"4rem"} height={"4rem"} />
+            <p className="text-4xl">{niñas}</p>
+          </div>
+          <p className="text-center w-full font-semibold">Niñas</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+interface ContadorResultadosProps {
+  niños: string;
+  niñas: string;
+}
+function ContadorResultados(props: ContadorResultadosProps) {
   const { niños, niñas } = props;
   return (
     <div className="z-40 flex flex-row w-full gap-4 items-center justify-center">
